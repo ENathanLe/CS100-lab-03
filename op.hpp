16c4d7aea05118aa20bb49d46cf8eaaf69d15fa9
nlee097 <nlee097@ucr.edu>
2021-04-20 02:09:33 -0700

    complete op unit testing and minor fixes to op.hpp

--- op.hpp
+++ op.hpp
@@ -11,10 +11,13 @@ class Op : public Base {
 		std::string truncStr = std::to_string(value);
 		if(value != 0){
 			std::size_t found = truncStr.find_last_not_of("0");
-			if(found!=std::string::npos) truncStr.erase(found+1);
+			if(found!=std::string::npos) truncStr.erase(found+1);  //remove any trailing 0's
 			else truncStr.clear();
+                        found = truncStr.find_last_not_of(".");
+                        if(found!=std::string::npos) truncStr.erase(found+1);  //remove any trailing decimal point
+                        else truncStr.clear();
 		}else
-			truncStr = "0.0";
+			truncStr = "0";
 		return truncStr;
 	}
     protected:
--- op_test.hpp
+++ op_test.hpp
@@ -11,11 +11,35 @@ TEST(OpTest, OpEvaluateNonZero) {
     EXPECT_EQ(test->evaluate(), 8);
 }
 
+TEST(OpTest, OpEvaluateNeg) {
+    Op* test = new Op(-5.67);
+    EXPECT_EQ(test->evaluate(), -5.67);
+}
+
+TEST(OpTest, OpEvaluateZero) {
+    Op* test = new Op(0);
+    EXPECT_EQ(test->evaluate(), 0);
+}
+
 TEST(OpTest, OpString) {
     Op* test = new Op(3.4);
     std::string test_str = test->stringify();
     EXPECT_EQ(test_str, "3.4");
 }
 
+TEST(OpTest, OpStringifyFraction) {
+    Op* test = new Op(0.25);
+    EXPECT_EQ(test->stringify(), "0.25");
+}
+
+TEST(OpTest, OpStringifyNeg) {
+    Op* test = new Op(-10.0);
+    EXPECT_EQ(test->stringify(), "-10");
+}
+
+TEST(OpTest, OpStringifyZero) {
+    Op* test = new Op(0);
+    EXPECT_EQ(test->stringify(), "0");
+}
 
 #endif //__OP_TEST_HPP__
