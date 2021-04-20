72043138b4a87c37abaf3e68f4942ade527dec22
toothnnail <michael.s.dong@gmail.com>
2021-04-20 12:13:56 -0700

    Added stringify test

--- toothnnail_tests.hpp
+++ toothnnail_tests.hpp
@@ -30,3 +30,16 @@ TEST(ToothnnailTest, AddMultDivSub) {
 	Base* sub = new Sub(add, val5);
 	EXPECT_EQ(sub->evaluate(), -2);	
 }//2 + 3 * 5 / -5 - 1
+
+TEST(ToothnnailTest, AddMultDivSubString) {
+	Base* val1 = new Op(2);
+	Base* val2 = new Op(3);
+	Base* val3 = new Op(5);
+	Base* val4 = new Op(-5);
+	Base* val5 = new Op(1);
+	Base* mult = new Mult(val2, val3);
+	Base* div = new Div(mult, val4);
+	Base* add = new Add(val1, div);
+	Base* sub = new Sub(add, val5);
+	EXPECT_EQ(sub->stringify(), "((2 + ((3 * 5) / -5)) - 1)");	
+}//2 + 3 * 5 / -5 - 1
